ba3344c02c71dc709cee0b58436c562951c55e56
Juliana <Juliana@DESKTOP-P5BPDTB>
2019-12-10 16:06:14 -0300

    teste

--- src/env/homer/CloudVision.java
+++ src/env/homer/CloudVision.java
@@ -44,7 +44,7 @@ public class CloudVision {
 	          .build();
 	      requests.add(request);
 
-	      // Performs label detection on the image file
+	      // Performs label detection on the image file dfsdfsdfsdf
 	      BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
 	      List<AnnotateImageResponse> responses = response.getResponsesList();
 
