fe3ebc152cfb54b0aadb254fbb6b44121882a8ea
Davi-K-Silva <davi.KDS.01@gmail.com>
2020-09-30 16:50:41 -0300

    otimização

--- src/env/homer/lightsControl/CheckLights.java
+++ src/env/homer/lightsControl/CheckLights.java
@@ -8,56 +8,51 @@ import java.net.http.HttpResponse;
 import com.google.gson.Gson;
 
 class ReturnValues {
-	private boolean Light;
+	private int return_value;
 	ReturnValues(){	
 	}
-	public boolean getLight() {
-		return Light;
+	public int getLight() {
+		return return_value;
 	}
 }
 
 public class CheckLights {
 
-	private static String ESP32IP = "ESP32_IP"; // Inserir o IP do ESP32 exibido no console da IDE da placa
+	private static String ESP32IP = "192.168.0.33"; // Inserir o IP do ESP32 exibido no console da IDE da placa
 	
 	public static String check() {
 		try {
 			String status = null;
-			// Envia uma solicitaÁ„o de variavel para o ESP32
+			// Envia uma solicita√ß√£o de variavel para o ESP32
 			System.out.print("Enviando Request ao ESP32... ");
        	 	HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder() // Request  para  atualizar a valor do sensor 
+            HttpRequest request = HttpRequest.newBuilder() // Request para atualizar e retornar valor do sensor 
                     .uri(URI.create("http://" + ESP32IP + "/checkLight")) 
-                    .build();
-            HttpRequest request2 = HttpRequest.newBuilder() // Request para retornar o valor do sensor
-                    .uri(URI.create("http://" + ESP32IP + "/Light")) 
                     .build();
             HttpResponse<String> response = client.send(request,
             			HttpResponse.BodyHandlers.ofString());
-            HttpResponse<String> response2 = client.send(request2,
-        			HttpResponse.BodyHandlers.ofString());
             
             System.out.println("Pronto!");
             
             // Organiza a resposta em JSON
             Gson gson = new Gson();
-            ReturnValues Jresponse = gson.fromJson(response2.body(), ReturnValues.class);
+            ReturnValues Jresponse = gson.fromJson(response.body(), ReturnValues.class);
             
             // Verifica a variavel e exibe a resposta
-            if(Jresponse.getLight()) {
+            if(Jresponse.getLight() == 0) {
             	status = "On";
-            	System.out.println("A luz est· ligada");
-            } else if(!Jresponse.getLight()) {
+            	System.out.println("A luz est√° ligada");
+            } else if(Jresponse.getLight() == 1) {
             	status = "Off";
-            	System.out.println("A luz est· desligada");
+            	System.out.println("A luz est√° desligada");
             } else {
             	status = "Erro";
-            	System.out.println("N„o foi possivel reconhecer o estado da luz");
+            	System.out.println("N√£o foi possivel reconhecer o estado da luz");
             }
             return status;
        } catch (Exception e) {
-    	    System.out.printf("\n N„o foi possivel conectar ao ESP32");
-    	   // e.printStackTrace();
+    	    System.out.printf("\n N√£o foi possivel conectar ao ESP32");
+    	    // e.printStackTrace();
     	    return "Erro";
        }
 	}
