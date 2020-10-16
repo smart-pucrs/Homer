db5b231eab47cdb87029853a144c7b07937713a1
Davi-K-Silva <davi.KDS.01@gmail.com>
2020-10-16 10:42:41 -0300

    Forecast update build error corrected

--- src/env/homer/weather/OPWtest.java
+++ src/env/homer/weather/OPWtest.java
@@ -1,21 +1,24 @@
 package homer.weather;
+
+import homer.weather.OpWeatherAPI.DayOutOfRangeException;
+
 public class OPWtest { // Classe de teste para exemplificar o uso da API e classe auxiliar criada
-	public static void main (String[] args) {
+	public static void main (String[] args) throws DayOutOfRangeException {
 		OpWeatherAPI wStation = new OpWeatherAPI("01dc0885a94ac05dd3185df11da3d3fc");
 		System.out.println("| Exemplos  ========================================================= |");
-		System.out.println("Usu·rio: Boa noite Homer");
+		System.out.println("Usu√°rio: Boa noite Homer");
 		System.out.println("Homer: Boa noite, "  + wStation.boaNoite());
 		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("Usu·rio: Bom dia Homer");
+		System.out.println("Usu√°rio: Bom dia Homer");
 		System.out.println("Homer: Bom dia, "  + wStation.bomDia());
 		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("Usu·rio: Homer qual a previs„o para o dia 2?");
+		System.out.println("Usu√°rio: Homer qual a previs√£o para o dia 2?");
 		System.out.println("Homer: " + wStation.previsaoDia(2));
 		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("Usu·rio: Homer vai chover amanh„ ?");
+		System.out.println("Usu√°rio: Homer vai chover amanh√£ ?");
 		System.out.println("Homer: " + wStation.choverAmanha());
 		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("Usu·rio: Homer quando o sol se pıe ?");
+		System.out.println("Usu√°rio: Homer quando o sol se p√µe ?");
 		System.out.println("Homer: " + wStation.tempoDeSol());
 		System.out.println("-----------------------------------------------------------------------");
 			
